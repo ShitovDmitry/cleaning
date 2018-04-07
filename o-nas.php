6fa1b3095f256bf8b3e9b1a2267ef72f1a7e8327
Sdmitry <shitovdmitry27@gmail.com>
2018-04-07 17:04:35 +0300

    123

--- o-nas.php
+++ o-nas.php
@@ -1,2 +1,156 @@
 <?php require_once $_SERVER["DOCUMENT_ROOT"]."/header.php"?>
+
+<div id="about-main">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<img src="/assets/img/LOGO.png">
+			</div>
+			<div class="col-lg-8">
+				<div class="title">
+					Чистая победа — современная клининговая компания
+				</div>
+				<div class="subtitle">
+					Мы профессионально убираем жилые и рабочие помещения, а также поддерживаем в них чистоту по удобному для вас графику.
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 description">
+				Мы убеждены, что работать надо не «потому, что надо», а только по призванию и со стремлением сделать мир лучше. Наша дружная команда — это не просто профессионалы, безупречно владеющие навыками клининга, работы со специальными средствами и покрытиями, техникой и инвентарём.
+
+				Прежде всего, наша команда объединила тех, для кого уборка квартир и домов, глажка белья, мытьё окон — это удовольствие и горячо любимое дело. И уж поверьте, такие люди действительно есть! Блеск свежевымытых стёкол, опрятность и чистота, улыбки на лицах хозяев — всё это безмерно радует их.
+			</div>
+			<div class="block-border primari-border">
+				Мы предоставляем активным людям возможность поддерживать чистоту и здоровую атмосферу дома и на работе без затрат времени и сил.
+			</div>
+		</div>
+	</div>
+</div>
+<div id="team">
+	<div class="container">
+		<div class="header-block  with-border">
+			КОМАНДА И ПЕРСОНАЛ
+		</div>
+		<div class="body-block">
+			<div class="text">
+				Во всех отраслях необходимы специалисты. Уборка давно стала делом профессионалов. Их услугами пользуются не только крупные организации, но и частные лица. Плюсы специалиста по клинингу – быстрота и грамотное использование чистящих средств.
+			</div>
+			<div class="testimonials-team-list">
+				<div class="item-testimonial col-lg-4">
+					<img src="">
+					<div calss="title">
+						ОТБОР КАНДИДАТОВ
+					</div>
+					<div class="description">
+						<ul>
+							<li>
+								Подтвержденный опыт работы в клининге от 8 лет
+							</li>
+							<li>
+								Проверка службой безопасности
+							</li>
+							<li>
+								Проверка состояния здоровья
+							</li>
+							<li>
+								Проверка на полиграфе
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="item-testimonial col-lg-4">
+					<img src="">
+					<div calss="title">
+						ОБУЧЕНИЕ ПЕРСОНАЛА
+					</div>
+					<div class="description">
+						<ul>
+							<li>Ежеквартальное обучение по профилю работы</li>
+							<li>Сертификация от производителей оборудования и чистящих средств</li>
+						</ul>
+					</div>
+				</div>
+				<div class="item-testimonial col-lg-4">
+					<img src="">
+					<div calss="title">
+						ОЦЕНКА КАЧЕСТВА
+					</div>
+					<div class="description">
+						<ul>
+							<li>Собственная многоуровневая система оценки качества работы</li>
+							<li>Размер оплаты труда привязан к оценке качества работы клиентом</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="about-additional-text-block">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 hidden-xs"></div>
+			<div class="col-lg-6 text-block">
+				Не секрет, что дома нынче бывают разные: от небольшой квартиры или домика — до бизнес-центра, целых турбаз и гостиничных комплексов на лоне природы.
+
+				Отсюда и главный принцип нашей работы — всегда и во всём ориентироваться на клиента и его потребности. И это означает не только доступные, качественные и оперативные клининговые услуги. Это ещё и гибкая, мобильная система работы, эффективная обратная связь, внимание ко всем пожеланиям и требованиям.
+
+				Любые отзывы клиентов о нашей работе необычайно важны. Чтобы обеспечить должный уровень услуг, мы постоянно учимся, практикуемся, регулярно проходим повышение квалификации.
+			</div>
+		</div>
+	</div>
+</div>
+<div id="testimonials">
+	<div class="container">
+		<div class="header-block with-border">
+			НАШИ КЛИЕНТЫ
+		</div>
+		<div class="body-block">
+			<div class="yotube-video row">
+				<div class="col-lg-6">
+					<img src="/assets/img/youtube-frame.png">
+				</div>
+				<div class="col-lg-6">
+					<img src="/assets/img/youtube-frame.png">
+				</div>
+			</div>
+			<div class="partners row">
+				<div class="partner-item col-lg-3 col-md-6">
+					<img src="/assets/img/p-1.png">
+				</div>
+				<div class="partner-item col-lg-3 col-md-6">
+					<img src="/assets/img/p-2.png">
+				</div>
+				<div class="partner-item col-lg-3 col-md-6">
+					<img src="/assets/img/p-3.png">
+				</div>
+				<div class="partner-item col-lg-3 col-md-6">
+					<img src="/assets/img/p-4.png">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="callback-block">
+	<div class="container">
+		<div class="body-block">
+			<form class="call-back-form">
+				<div class="text">Свяжитесь с нами ПРЯМО СЕЙЧАС и мы перезвоним Вам в течение 15 минут! </div>
+				<div class="inline-form-group">
+					<div class="form-group">
+						<input class="form-control" type="text" name="name" id="phone" placeholder="Имя">
+					</div>
+					<div class="form-group">
+						<input class="form-control" type="text" name="phone" id="phone" placeholder="Телефон">
+					</div>
+					<div class="form-group">
+						<button class="btn btn-primary" type="submit">БЕСПЛАТНЫЙ РАСЧЕТ</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
 <?php require_once $_SERVER["DOCUMENT_ROOT"]."/footer.php"?>
