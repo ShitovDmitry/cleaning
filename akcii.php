c2728a126cd26534fb4f3c493a81f4de45a6d815
Sdmitry <shitovdmitry27@gmail.com>
2018-04-09 13:05:44 +0300

    t

--- akcii.php
+++ akcii.php
@@ -1,2 +1,203 @@
 <?php require_once $_SERVER["DOCUMENT_ROOT"]."/header.php"?>
+<div id="vertikal-tab-block">
+	<div class="container">
+		<div class="header-block with-border">
+			СКИДКИ И АКЦИИ
+		</div>
+		<div class="body-block">
+			<div class="horizontal-tab-list">
+				<div class="tab-header-block">
+					<div class="item-tab" id="one">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								СКИДОЧНАЯ КАРТА
+							</div>
+							<div class="description">
+								При первом заказе Вы получаете в подарок пластиковую карту со скидкой 10% на все виды услуг от кампании "ЧИСТАЯ ПОБЕДА".
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="two">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Подарочный сертификат
+							</div>
+							<div class="description">
+								Подарочный сертификат на проведение любых работ, которые осуществляет наша компания
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="three">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Приведи друга и получите
+								скидку 500 рублей!
+							</div>
+							<div class="description">
+								Делитесь приятными впечатлениями о «Чистая победа»! Это выгодно.
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="four">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Скидка 500 рублей за опоздание
+							</div>
+							<div class="description">
+								Если Вы сделали заказ в нашей компании, время проведения которого утром или в первой половине дня, а сотрудник нашей компании опоздал более чем на 15 минут,
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="five">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Мир. Дружба. Жвачка.
+							</div>
+							<div class="description">
+								Скидки при заключении постоянного договора
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="six">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Скидка 50% на химчистку
+								мягкой мебели
+							</div>
+							<div class="description">
+								Скидка 50% на химчистку мягкой мебели при заказе генеральной уборки.
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="seven">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Влажная уборка в подарок
+							</div>
+							<div class="description">
+								Влажная уборка в подарок при заказе общей послестроительной уборки загородного дома/коттеджа.
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="eight">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								месяц уборки БЕСПЛАТНО
+							</div>
+							<div class="description">
+								Вы получаете 1 месяц уборки СОВЕРШЕННО БЕСПЛАТНО! При заключении годового контракта на обслуживание офиса
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="nine">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Влажная уборка в подарок
+							</div>
+							<div class="description">
+								Порекомендуйте нас своим партнерам или друзьям и вы получите специальные условия обслуживания в следующем месяце
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="ten">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Гарантия лучшей цены
+							</div>
+							<div class="description">
+								Мы предлагаем вам стоимость на 5% ниже той, которую Вы платите нашим конкурентам!
+							</div>
+						</div>
+					</div>
+					<div class="item-tab" id="eleven">
+						<div class="pull-left">
+							<img src="/assets/img/">
+						</div>
+						<div class="content">
+							<div class="title">
+								Генеральная уборка офиса
+								бесплатно
+							</div>
+							<div class="description">
+								Оплатите услуги по уборке офиса за 3 месяца вперед и получите бонус - генеральная уборка офиса бесплатно (2 уборки бесплатно)
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-contants-block">
+					<div class="tab-content" data-tab="one">
+						<img src="/assets/img/">
+						<div class="head-title">
+
+						</div>
+						<div class="description">
+
+						</div>
+						<button class="btn btn-primary">ПОЛУЧИТЬ СКИДКУ</button>
+					</div>
+					<div class="tab-content" data-tab="two">
+
+					</div>
+					<div class="tab-content" data-tab="three">
+
+					</div>
+					<div class="tab-content" data-tab="four">
+
+					</div>
+					<div class="tab-content" data-tab="five">
+
+					</div>
+					<div class="tab-content" data-tab="six">
+
+					</div>
+					<div class="tab-content" data-tab="seven">
+
+					</div>
+					<div class="tab-content" data-tab="eight">
+
+					</div>
+					<div class="tab-content" data-tab="nine">
+
+					</div>
+					<div class="tab-content" data-tab="ten">
+
+					</div>
+					<div class="tab-content" data-tab="eleven">
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php require_once $_SERVER["DOCUMENT_ROOT"]."/footer.php"?>
